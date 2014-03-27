f1d83a781ddc39114989d1d9f0babd6673f62f48
Projeto Fazenda <drigodev@gmail.com>
2014-03-26 23:41:25 -0300

    Iniciando sistema

---
+++ categoria.php
@@ -0,0 +1,4 @@
+Bovino: <br>
+ID: <input type="text" width="100px" readonly><br>
+Categoria: <input type="text" width="100px" >|Ativo:<input type="checkbox"><br>
+<input type="button" value="Salvar">   <input type="button" value="Cancelar">
---
+++ frmPrincipal.php
@@ -0,0 +1,95 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <title>Sistema Fazenda Feliz</title>
+    <link type="text/css" href="css/menu.css" rel="stylesheet" />
+    <script type="text/javascript" src="js/Jquery.js"></script>
+	<script type='text/javascript'>
+    function carrega(sPagina){
+      $("#divPrincipal").load(sPagina);		
+    }
+    
+    function padrao(){
+    $("#divPrincipal").height = 900;
+        
+    };
+    </script>
+</head>
+<body onLoad="padrao();">
+
+<style type="text/css">
+* { margin:0;
+    padding:0;
+}
+
+#menu { top:50px; }
+#copyright {
+    width:100%;
+    font:12px 'Trebuchet MS';
+    color:#bbb;
+    text-indent:20px;
+    text-align:right;
+	vertical-align:bottom; 
+}
+#copyright a { color:#bbb; }
+#copyright a:hover { color:#fff; }
+</style>
+
+<div id="menu">
+    <ul class="menu">
+        <li><a href="http://localhost/jonas/frmPrincipal.php"><span>Inicial</span></a></li>
+        <li><a href="#" class="parent"><span>Cadastros</span></a>
+            <div><ul>
+                <li><a href="#" onClick="carrega('categoria.php')"><span>Categoria</span></a></li>
+                <li><a href="#" onClick="carrega('bovino.php')"><span>Bovino</span></a></li>
+                <li><a href="#"><span>Marca</span></a></li>
+                <li><a href="#"><span>Unidade</span></a></li>
+                <li><a href="#"><span>Produto</span></a></li>
+                <li><a href="#"><span>Setor</span></a></li>
+                <li><a href="#"><span>Vacina</span></a></li>
+            </ul></div>
+        </li>
+        
+        <li><a href="#" class="parent"><span>Lançamentos</span></a>
+            <div><ul>
+                <li><a href="#"><span>Compra</span></a></li>
+                <li><a href="#"><span>Venda</span></a></li>     
+                <li><a href="#"><span>Registro Vacina</span></a></li>     
+                </ul>
+            </div>
+        </li>    
+        
+        <li><a href="#" class="parent"><span>Relatórios</span></a>
+            <div class="columns two">
+                <ul class="one">
+                    <li><a href="#"><span>Sub Item 1.1</span></a></li>
+                    <li><a href="#"><span>Sub Item 1.2</span></a></li>
+                    <li><a href="#"><span>Sub Item 1.3</span></a></li>
+                    <li><a href="#"><span>Sub Item 1.4</span></a></li>
+                    <li><a href="#"><span>Sub Item 1.5</span></a></li>
+                    <li><a href="#"><span>Sub Item 1.6</span></a></li>
+                    <li><a href="#"><span>Sub Item 1.7</span></a></li>
+                </ul>
+                <ul class="two">
+                    <li><a href="#"><span>Sub Item 2.1</span></a></li>
+                    <li><a href="#"><span>Sub Item 2.2</span></a></li>
+                    <li><a href="#"><span>Sub Item 2.3</span></a></li>
+                    <li><a href="#"><span>Sub Item 2.4</span></a></li>
+                    <li><a href="#"><span>Sub Item 2.5</span></a></li>
+                    <li><a href="#"><span>Sub Item 2.6</span></a></li>
+                </ul>
+            </div>
+        </li>
+        <li><a href="#"><span>Sobre</span></a></li>
+        <li class="last"><a href="#"><span>Versão 1.0</span></a></li>
+    </ul>
+</div>
+<div id="divPrincipal" style="margin-top:40px">
+
+</div>
+<div id="copyright">Copyright &copy; 2014 <a href="">Sistemas Maninho</a></div>
+
+</body>
+</html>
